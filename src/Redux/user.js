db254e37e9bf712c25f10a3c23f73919838b17c9
Stanley Sham <stanley.sham94@gmail.com>
2020-09-08 11:36:50 -0400

    login function using redux complete

--- src/Redux/user.js
+++ src/Redux/user.js
@@ -1,13 +1,16 @@
+import userService from '../utils/userService';
+
 const LOGIN_USER = 'LOGIN_USER';
 const LOGOUT_USER = 'LOGOUT_USER';
 const SIGNUP_USER = 'SIGNUP_USER';
 
 export const loginUser = () => ({
         type: LOGIN_USER,
-        payload: null
+        payload: userService.getUser()
 })
 
 export const logoutUser = () => {
+    userService.logout();
     return {
         type: LOGOUT_USER
     }
@@ -15,10 +18,10 @@ export const logoutUser = () => {
 
 export const signupUser = () => ({
     type: SIGNUP_USER,
-    payload: null
+    payload: userService.getUser()
 })
 
-function userReducer(state = null, action) {
+function userReducer(state = userService.getUser(), action) {
     switch(action.type) {
         case LOGIN_USER:
             return action.payload;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import './LoginPage.css';
 import userService from '../../utils/userService';
+import { connect } from 'react-redux';
+import { loginUser } from '../../Redux/user'
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -14,98 +16,123 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+
+
 const useStyle = makeStyles((theme) => ({
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    justifyContent: "center"
-  },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        justifyContent: "center"
+    },
 }));
+
+function formReducer(state, action) {
+    switch (action.type) {
+        case 'UPDATE_INPUT':
+            return {
+                ...state,
+                [action.payload.name]: action.payload.value
+            };
+        default:
+            throw new Error(`Unsupported action ${action.type}`);
+    }
+}
 
 
 function LoginPage(props) {
-  const [state, setState] = useState({email: '', pw: ''})
-  
-  let handleChange = (e) => {
-    const {name, value} = e.target;
-    setState(prevState => ({...prevState, [name]: value}));
-  }
-
-  let handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await userService.login(state);
-      props.handleSignupOrLogin();
-      props.history.push('/');
-    } catch (err) {
-      alert('Invalid Credentials!');
+    const initialState = {
+        email: '', 
+        pw: ''
     }
-  }
 
-  const classes = useStyle();
+    const [info, setInfo] = useReducer(formReducer, initialState);
 
-    return (
-<Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className="paper">
-        <Avatar className="avatar">
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className="form" noValidate onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            onChange={handleChange}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="pw"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            onChange={handleChange}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container justify="space-between">
-            <Grid item>
-              <Link href="/" variant="body2">
-                Cancel
-              </Link>
+    function handleChange(e) {
+        setInfo({
+            type: 'UPDATE_INPUT',
+            payload: e.target
+        });
+    }
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await userService.login(info);
+            props.handleSignupOrLogin();
+            props.history.push('/');
+        } catch (err) {
+            alert('Invalid Credentials!');
+        }
+    }
+
+    const classes = useStyle();
+
+        return (
+    <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className="paper">
+            <Avatar className="avatar">
+            <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+            Sign in
+            </Typography>
+            <form className="form" noValidate onSubmit={handleSubmit}>
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={handleChange}
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="pw"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                onChange={handleChange}
+            />
+            <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+            >
+                Sign In
+            </Button>
+            <Grid container justify="space-between">
+                <Grid item>
+                <Link href="/" variant="body2">
+                    Cancel
+                </Link>
+                </Grid>
+                <Grid item>
+                <Link href="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                </Link>
+                </Grid>
             </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-    );
-  }
+            </form>
+        </div>
+        </Container>
+        );
+    }
 
-export default LoginPage;
+    const mapDispatchToProps = (dispatch) => ({
+        loginUser: () => dispatch(loginUser())
+    });
+
+    export default connect(null, mapDispatchToProps) (LoginPage);
