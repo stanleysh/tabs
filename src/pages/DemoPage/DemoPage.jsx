35b920bbe29b38aaf0eac81b584d8609b097492f
Stanley Sham <stanley.sham94@gmail.com>
2020-09-08 21:54:00 -0400

    fixed useeffect ono demopage

--- src/pages/DemoPage/DemoPage.jsx
+++ src/pages/DemoPage/DemoPage.jsx
@@ -5,6 +5,7 @@ import expenseService from '../../utils/expenseService';
 function DemoPage({history}) {
     const [totalAmount, setTotalAmount] = useState();
     const [currentAmount, setCurrentAmount] = useState();
+    const [date, setDate] = useState([])
 
     useEffect(() => {
         // when searching up specific users use the following below:
@@ -12,13 +13,26 @@ function DemoPage({history}) {
         async function getCost() { 
             const result = await expenseService.getMonthlyCost('/api/expenses')
             setTotalAmount(result.total_amount)
+            return
         };
+
+        function getCurrentDate() {
+            const months = {0:'January', 1:'February', 2:'March', 3:'April', 4:'May', 5:'June', 6:'July', 7:'August', 8: 'September', 9:'October', 10: 'November', 11: 'December'}
+            let d = new Date()
+            let currentMonth = d.getMonth()
+            let currentYear = d.getFullYear()
+            setDate([months[currentMonth], currentYear])
+            return
+        }
+
         getCost();
-    }, [])
+        getCurrentDate();
+    }, [date])
 
     return (
         <div className = 'month-preview'>
-            <h1>Quick View</h1>
+            <h1>Quick View for {date[0]}, {date[1]}</h1>
+            <h2>{date[0]} Expenditures: </h2>
             <h2>First Chart {totalAmount}</h2>
             {/* <h2>Second chart {currentAmount}</h2> */}
         </div>
