b49046946b29f51d15de0227038d5761034e5c0d
Stanley Sham <stanley.sham94@gmail.com>
2020-09-24 21:13:34 -0400

    fixing eslint issues

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,71 +1,70 @@
 // Updating expenses using total amount from expense table
-// WITH total as (SELECT user_id, SUM(amount) as total_expense FROM expenses GROUP BY user_id) UPDATE users SET current_funds = total.total_expense FROM total WHERE users.id = total.user_id
+// WITH total as (SELECT user_id, SUM(amount) as total_expense FROM expenses GROUP BY user_id)
+// UPDATE users SET current_funds = total.total_expense FROM total WHERE users.id = total.user_id
 
 const { pool } = require('../config/database');
 
 const getExpenses = (req, res) => {
-    pool.query('SELECT * FROM expenses', (error, results) => {
-        if (error){
-            throw error
-        }
-        res.status(200).json(results.rows);
-    })
-}
+  pool.query('SELECT * FROM expenses', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
 
 // pool.query('SELECT categories.category_name, expenses.name, expenses.amount FROM expenses, categories WHERE expenses.user_id = $1 AND  categories.user_id = $1', [id], (error, results) => {
 
-
 const getUserExpenses = (req, res) => {
-    const id = parseInt(req.params.id)
-    pool.query('SELECT category_name, name, amount, expense_date::date FROM expenses INNER JOIN categories ON categories.id = expenses.category_id WHERE expenses.user_id = $1', [id], (error, results) => {
-        if (error) {
-            res.status(400).json(error)
-        } else {
-            res.status(200).json(results.rows)
-        }
-    })
-}
+  const id = parseInt(req.params.id)
+  pool.query('SELECT category_name, name, amount, expense_date::date FROM expenses INNER JOIN categories ON categories.id = expenses.category_id WHERE expenses.user_id = $1', [id], (error, results) => {
+    if (error) {
+      res.status(400).json(error);
+    } else {
+      res.status(200).json(results.rows);
+    }
+  });
+};
 
 const createExpense = (req, res) => {
-    const user_id = parseInt(req.params.id)
-    pool.query('INSERT INTO expenses (user_id, categories_id, name, amount, expense_date) VALUES ($1, $2, $3, $4, $5)', [user_id, req.body.category_id, req.body.name, req.body.amount, req.body.expense_date], (error, newExpense) => {
-        if (error) {
-            console.log(error)
-        } else {
-            res.json({msg: 'Expense created'});
-        }
-    })
-}
+  const user_id = parseInt(req.params.id)
+  pool.query('INSERT INTO expenses (user_id, categories_id, name, amount, expense_date) VALUES ($1, $2, $3, $4, $5)', [user_id, req.body.category_id, req.body.name, req.body.amount, req.body.expense_date], (error, newExpense) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json({msg: 'Expense created'});
+    }
+  });
+};
 
 const getMonthlyAndTotal = (req, res) => {
-    if (req.params.id) {
-        var user_id = parseInt(req.params.id)
-    } else {
-        var user_id = 1
+  if (req.params.id) {
+    const user_id = parseInt(req.params.id);
+  } else {
+    const user_id = 1;
+  }
+  pool.query('SELECT(SELECT SUM(amount) FROM expenses WHERE user_id = $1) AS total_amount, (SELECT SUM(amount) FROM expenses WHERE user_id = $1 AND date_trunc(\'month\', expense_date) = date_trunc(\'month\', CURRENT_DATE) AND date_trunc(\'year\', expense_date) = date_trunc(\'year\', CURRENT_DATE)) AS current_amount', [user_id], (error, result) =>{
+    if (error) {
+      console.log(error);
     }
-    pool.query('SELECT(SELECT SUM(amount) FROM expenses WHERE user_id = $1) AS total_amount, (SELECT SUM(amount) FROM expenses WHERE user_id = $1 AND date_trunc(\'month\', expense_date) = date_trunc(\'month\', CURRENT_DATE) AND date_trunc(\'year\', expense_date) = date_trunc(\'year\', CURRENT_DATE)) AS current_amount', [user_id], (error, result) =>{
-        if (error) {
-            console.log(error)
-        } 
-        res.json(result.rows[0])
-    })
-}
+    res.json(result.rows[0]);
+  });
+};
 
 const getDemoMonth = (req, res) => {
     // pool.query('SELECT(SELECT SUM(amount) FROM expenses WHERE user_id = 1) AS total_amount, (SELECT SUM(amount) FROM expenses WHERE user_id = 1 AND expense_date >= date_trunc(\'month\', CURRENT_DATE)) AS current_amount', (error, result) =>{
-    pool.query('SELECT(SELECT SUM(amount) FROM expenses WHERE user_id = 1) AS total_amount, (SELECT SUM(amount) FROM expenses WHERE user_id = 1 AND EXTRACT(MONTH FROM expense_date) = 7) AS current_amount', (error, result) =>{
-        if (error) {
-            console.log(error)
-        } 
-        res.status(200).json(result.rows[0])
-    })
-}
+  pool.query('SELECT(SELECT SUM(amount) FROM expenses WHERE user_id = 1) AS total_amount, (SELECT SUM(amount) FROM expenses WHERE user_id = 1 AND EXTRACT(MONTH FROM expense_date) = 7) AS current_amount', (error, result) =>{
+    if (error) {
+      console.log(error);
+    }
+    res.status(200).json(result.rows[0]);
+  });
+};
 
 module.exports = {
-    getExpenses,
-    getUserExpenses,
-    createExpense,
-    getMonthlyAndTotal,
-    getDemoMonth,
-}
-
+  getExpenses,
+  getUserExpenses,
+  createExpense,
+  getMonthlyAndTotal,
+  getDemoMonth,
+};
--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = require('express').Router();
-const expensesCtrl = require ('../../controllers/expenses');
+const expensesCtrl = require('../../controllers/expenses');
 
 // Has check auth, remember to put this back in after testing
 // router.get('/:id', checkAuth, expensesCtrl.getUserExpenses);
-
 
 // for testing only, remember to delete later !!!
 router.get('/', expensesCtrl.getExpenses);
@@ -13,12 +12,9 @@ router.get('/monthly/:id', expensesCtrl.getMonthlyAndTotal);
 router.get('/:id', expensesCtrl.getUserExpenses);
 router.post('/:id/expense', expensesCtrl.createExpense);
 
-
-
 function checkAuth(req, res, next) {
-    if (req.user) return next();
-        return res.status(401).json({msg: 'Not Authorized'});
+  if (req.user) return next();
+  return res.status(401).json({ msg: 'Not Authorized' });
 }
 
-
-module.exports = router
+module.exports = router;
